c34d065c8816e6fa14a0b159aae5ba6a104b61c4
RichieHeig <momorichard2000@yahoo.fr>
2026-01-23 22:25:10 +0000

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://phojtiaesozznmlaxrl.supabase.co'
+const supabaseUrl = 'https://phojtiaeesozznnlaxrl.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBob2p0aWFlZXNvenpubmxheHJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc4NTg5NzUsImV4cCI6MjA4MzQzNDk3NX0.OREWne9nPKZdaRMWaTR3715LAor9I7jJ60JLaDrIFb8'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
